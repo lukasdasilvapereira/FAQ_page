744475d602439e2f4aaf7882ea8b1d4a1ce13664
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-12 18:50:49 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let other1 = document.querySelector(".other1");
+let other2 = document.querySelector(".other2");
+let other3 = document.querySelector(".other3");
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+
+function clicar() {
+    other1.style.display = "block";
+}
